7a7dc08415eb207fdfb31dbd5244f47a73de043c
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-21 16:33:43 +0530

    hide form functionality added .

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const overlay = document.getElementById("overlay");
 const showFormBtn = document.getElementById("show-form-btn");
-
+const hideFormButton = document.getElementById("close-form-btn");
 showFormBtn.addEventListener("click", ()=>{
     overlay.style.display = "flex";
+});
+hideFormButton.addEventListener("click", ()=>{
+    overlay.style.display = "none";
 });
