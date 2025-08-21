0f706171f17843a73eb82ae6753f569f4498c2e6
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-21 17:09:42 +0530

    api and functions added.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const showFormBtn = document.getElementById("show-form-btn");
 const hideFormButton = document.getElementById("close-form-btn");
 const noteForm = document.getElementById("note-form");
 
+const BASE_API_URL = "https://noteapp-ovs5.onrender.com/api/"
+
 // show form
 showFormBtn.addEventListener("click", ()=>{
     overlay.style.display = "flex";
@@ -40,3 +42,52 @@ noteForm.addEventListener("submit", (e) => {
         overlay.style.display = "none";
     }
 });
+
+
+// fetch all notes
+const fetchAllNotes = async ()=>{
+const res = await fetch(`${BASE_API_URL}notes`);
+const data = await res.json();
+console.log(data);
+return data;    
+};
+
+fetchAllNotes();
+
+
+// create note
+const createNote = async (noteData) => {
+    const res = await fetch(`${BASE_API_URL}note/create`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(noteData)
+    });
+    const data = await res.json();
+    console.log(data);
+    return data;
+};
+
+// edit note
+const editNote = async (noteId, noteData) => {
+    const res = await fetch(`${BASE_API_URL}note/update/${noteId}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(noteData)
+    });
+    const data = await res.json();
+    console.log(data);
+    return data;
+};
+// delete note
+const deleteNote = async (noteId) => {
+    const res = await fetch(`${BASE_API_URL}note/delete/${noteId}`, {
+        method: "DELETE"
+    });
+    const data = await res.json();
+    console.log(data);
+    return data;
+};
